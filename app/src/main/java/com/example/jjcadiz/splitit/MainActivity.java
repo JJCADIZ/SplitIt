5b0467836d13d3e4baac8822357881ec596b7148
JJCADIZ <raymundjcadiz@gmail.com>
2016-10-14 03:14:10 +0800

    Activities

--- app/src/main/java/com/example/jjcadiz/splitit/MainActivity.java
+++ app/src/main/java/com/example/jjcadiz/splitit/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.jjcadiz.splitit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,7 +13,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.dd.CircularProgressButton;
+import com.robinhood.ticker.TickerUtils;
+import com.robinhood.ticker.TickerView;
+
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -30,21 +32,18 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(getApplicationContext(), SplitActivity.class);
+                startActivity(intent);
+
             }
         });
 
-        //CIRCULAR BUTTON
-        final CircularProgressButton SplitButton = (CircularProgressButton) findViewById(R.id.btnWithText);
-        SplitButton.setIndeterminateProgressMode(true);
-        SplitButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                SplitButton.setProgress(100);
-                }
-            }
-        );
+        //TICKER
+        final TickerView tickerView = (TickerView) findViewById(R.id.ticker_bill);
+        tickerView.setCharacterList(TickerUtils.getDefaultNumberList());
+        tickerView.setCharacterList(TickerUtils.getDefaultListForUSCurrency());
+        tickerView.setText("$0");
+
 
         //LAYOUT
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -55,6 +54,14 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+    }
+
+    public void StartSplit (View view){
+        Intent split = new Intent(this, SplitActivity.class);
+        startActivity(split);
+
     }
 
     @Override
@@ -96,7 +103,8 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+            Intent intent = new Intent(getBaseContext(), SplitActivity.class);
+            startActivity(intent);
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
@@ -113,4 +121,8 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
 }
+
+
